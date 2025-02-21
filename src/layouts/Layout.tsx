6bcd71da82b4698f8db22ae8c67ffcf0feb6502d
ValeLan <vale.lanf@gmail.com>
2025-02-21 20:57:25 -0300

    Cree Las primeras rutas de la web y un componente Header.

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+
+            <main className="container mx-auto py-16">
+
+                <Outlet />
+            </main>
+
+        </>
+    )
+}
+
+export default Layout
---
+++ src/views/IndexPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+
+const IndexPage = () => {
+    return (
+        <>
+            <h1>Inicio</h1>
+        </>
+    )
+}
+
+export default IndexPage
